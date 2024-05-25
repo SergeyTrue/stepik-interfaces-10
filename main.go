06fa2a814a33278cc7904bd13669cfef0ad9268a
SergeyTrue <belousov.sd@gmail.com>
2024-05-25 11:31:01 +0000

    working with readTask function defenition

--- main.go
+++ main.go
@@ -11,18 +11,37 @@ func readTask()(interface{}, interface{},interface{}){
 }
 
 func main() {
+	var v1, v2 float64
+	var ok bool
+	var oper string
 	value1, value2, operation := readTask()
-	if v1, ok := value1.(float64); ok {
-		fmt.Printf("%f норм конвертировалось\n",v1)
+	if v1, ok = value1.(float64); ok {
+	} else {
+		fmt.Printf("value=%v: %T\n", value1, value1)
+		return
 	}
 
-	if v2, ok := value2.(float64); ok {
-		fmt.Printf("%f норм конвертировалось\n",v2)
+	if v2, ok = value2.(float64); ok {
+	} else {
+		fmt.Printf("value=%v: %T\n", value2, value2)
+		return
 	}
-	
-	if oper, ok := operation.(string); ok {
-		fmt.Printf("%s норм конвертировалось\n",oper)
+
+	if oper, ok = operation.(string); ok {
+		switch {
+			case oper == "+":
+				fmt.Printf("%.4f\n",v1+v2)
+			case oper == "-":
+				fmt.Printf("%.4f\n",v1-v2)
+			case oper == "*":
+				fmt.Printf("%.4f\n",v1*v2)
+			case oper == "/":
+				fmt.Printf("%.4f\n",v1/v2)
+			default: 
+				fmt.Println("неизвестная операция")
+		}
+	} else {
+	return
 	}
 }
-
 
