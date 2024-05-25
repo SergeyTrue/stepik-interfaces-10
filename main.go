d3d7e35a4d6d8238df15fb554355334060927ca8
SergeyTrue <belousov.sd@gmail.com>
2024-05-25 10:46:41 +0000

    Files swp addedd to .gitignore

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+//"encoding/json"
+//"os"
+)
+
+func readTask()(interface{}, interface{},interface{}){
+	return 12.5, 3.4, "+"
+}
 
 func main() {
-    // Ваш код здесь
-    fmt.Println("Hello, Stepik!")
+	value1, value2, operation := readTask()
+	if v1, ok := value1.(float64); ok {
+		fmt.Printf("%f норм конвертировалось\n",v1)
+	}
+
+	if v2, ok := value2.(float64); ok {
+		fmt.Printf("%f норм конвертировалось\n",v2)
+	}
+	
+	if oper, ok := operation.(string); ok {
+		fmt.Printf("%s норм конвертировалось\n",oper)
+	}
 }
+
+
