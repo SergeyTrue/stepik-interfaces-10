dfb2857b6d3ddde9e11759c5ce566cfd9c8901ab
SergeyTrue <belousov.sd@gmail.com>
2024-06-12 13:02:46 +0300

    parallelismStep13 added

---
+++ interfacesStep13/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	//"encoding/json"
+	//"os"
+)
+
+func readTask() (interface{}, interface{}, interface{}) {
+	return 12.5, 3.4, "+"
+}
+
+func main() {
+	value1, value2, operation := readTask()
+	var v1, v2 float64
+	var ok bool
+	var oper string
+	if v1, ok = value1.(float64); !ok {
+		fmt.Printf("value=%v: %T\n", value1, value1)
+		return
+	}
+
+	if v2, ok = value2.(float64); !ok {
+		fmt.Printf("value=%v: %T\n", value2, value2)
+		return
+	}
+
+	if oper, ok = operation.(string); ok {
+		switch {
+		case oper == "+":
+			fmt.Printf("%.4f\n", v1+v2)
+		case oper == "-":
+			fmt.Printf("%.4f\n", v1-v2)
+		case oper == "*":
+			fmt.Printf("%.4f\n", v1*v2)
+		case oper == "/":
+			fmt.Printf("%.4f\n", v1/v2)
+		default:
+			fmt.Println("неизвестная операция")
+		}
+	} else {
+		return
+	}
+}
